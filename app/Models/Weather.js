206de5fc49605057d616c08432d74b8bc4dabb4c
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-04-17 13:16:33 -0600

    actually fixed typo this time

--- app/Models/Weather.js
+++ app/Models/Weather.js
@@ -30,7 +30,7 @@ export class Weather
         <div>
             <img src="http://openweathermap.org/img/wn/${this.weatherIcon}@2x.png" title="${this.weatherText}" />
         </div>
-        <div class="d-flex flex-column text-center justify-content-center" tilte="Feels Like ${_CtoF(this.feelsLike).toFixed(1)}°F">
+        <div class="d-flex flex-column text-center justify-content-center" title="Feels Like ${_CtoF(this.feelsLike).toFixed(1)}°F">
             <span class="fs-3">${_CtoF(this.temperature).toFixed(1)}°F</span>
             <span class="on-hover">${_CtoF(this.low).toFixed(0)}°F | ${_CtoF(this.high).toFixed(0)}°F</span>
         <div>
